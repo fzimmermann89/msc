b3d7f5521848d158a0990075889611c82034da73
Felix Zimmermann <fzimmermann89@gmail.com>
2020-09-16 01:46:57 +0900

    add some files from sacla as backup

---
+++ h5util.py
@@ -0,0 +1,43 @@
+import numpy as np
+import h5py
+
+def appenddata(file, key, data):
+    data = np.atleast_1d(np.array(data))
+    if np.array(data).dtype.kind == 'U':
+        data = data.astype(h5py.string_dtype(encoding='ascii'))
+    if key not in file.keys():
+        file.create_dataset(key, chunks=data.shape, compression='lzf', shuffle=True, data=data, maxshape=(None, *data.shape[1:]))
+    else:
+        file[key].resize((file[key].shape[0] + data.shape[0]), axis=0)
+        file[key][-data.shape[0] :] = data
+
+def overwritedata(file, key, data, chunks=None):
+    data = np.atleast_1d(np.array(data))
+    if np.array(data).dtype.kind == 'U':
+        data = data.astype(h5py.string_dtype(encoding='ascii'))
+    if key in file.keys():
+        del file[key]
+    file.create_dataset(key,  chunks=chunks if not chunks is None else data.shape, compression='lzf',  shuffle=True, data=data, maxshape=(None, *data.shape[1:]))
+#compression='gzip',compression_opts=1,  shuffle=True,
+
+def shrink(file,key,n):
+    file[key].resize(file[key].shape[0]-n,axis=0)
+    
+def list2array(l):
+    maxlen=np.max([len(e) for e in l])
+    return np.array([np.pad(e,(0,maxlen-len(e)),'constant') for e in l])
+
+def copymasked(src, dst, mask):
+    def func(name, obj):
+        print('  ', name)
+        if isinstance(obj, h5py._hl.dataset.Dataset):
+            dst[name] = obj[mask, ...]
+
+    if isinstance(src, h5py._hl.dataset.Dataset):
+        name = src.name.split('/')[-1]
+        print('  ', name)
+        dst[name] = src[mask]
+    elif isinstance(src, h5py._hl.group.Group) or isinstance(h5py._hl.files.File):
+        src.visititems(func)
+    else:
+        raise TypeError
---
+++ imports.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+%matplotlib inline 
+import numpy as np
+import pandas as pd
+pd.options.display.max_rows = 999
+import scipy as sp
+import scipy.ndimage as snd
+import scipy.signal as ss
+import skimage.morphology as skm
+import mkl_fft
+import fast_histogram
+import sys, os, datetime, shutil, glob, itertools, collections, pathlib
+import h5py
+from tqdm.notebook import tqdm
+from pprint import pprint
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+%load_ext line_profiler
+%load_ext memory_profiler
+from sacla import *
+from accum import *
+import idi.reconstruction as recon
+
+
+def browse(obj,depth=0):
+    try:
+        if type(obj)==h5py._hl.dataset.Dataset:
+            print(obj.shape,obj.dtype,end='')
+            if np.product(obj.shape)<10: print(np.array(obj), end='')
+        else:
+            for k in obj.keys():
+                print('\n',depth*'   ',k,end='')
+                browse(obj[k],depth+1)
+            print()
+    except Exception as e:
+        print(e)
+
+def zoom(cor):
+        cor[cor.shape[0]//2,cor.shape[1]//2]=np.nan
+        plt.matshow(cor[cor.shape[0]//2-n:cor.shape[0]//2+n,cor.shape[1]//2-n:cor.shape[1]//2+n])
+    
