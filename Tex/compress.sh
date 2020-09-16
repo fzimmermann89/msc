f34018ea6d91ca06f6fdaf777dfd784157c76022
Felix Zimmermann <fzimmermann89@gmail.com>
2020-09-16 21:14:14 +0200

    tex: add compress.sh

---
+++ Tex/compress.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+gs -o compressed.pdf -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress  -dColorConversionStrategy=/LeaveColorUnchanged  -dDownsampleColorImages=false -dEmbedAllFonts=true  -dCompressFonts=true -dSubsetFonts=false -dNOPAUSE -dBATCH main.pdf
+rm main.pdf
+mv compressed.pdf main.pdf
